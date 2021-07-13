6faa14eb8d87cf71e1de70252cec3e09d5b21152
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-07-13 07:09:32 +0530

    🎉 let's add colors to commander.js!

---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+/*
+ *  Prettier run control for rupa project.
+ *  Created On 13 July 2021
+ */
+
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'lf',
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    parser: 'typescript',
+    quoteProps: 'as-needed',
+}
---
+++ src/rupa.ts
@@ -0,0 +1,6 @@
+/*
+ *  Entryfile for rupa library project.
+ *  Created On 13 July 2021
+ */
+
+console.log('hello')
