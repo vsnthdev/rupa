5abc4f0dc219c16e57e744954cef30814a42b9aa
Vasanth Developer <vsnthdev@gmail.com>
2022-08-09 22:56:47 +0530

    ✨ now attaches itself recursively to all cmds

--- src/rupa.ts
+++ src/rupa.ts
@@ -7,11 +7,22 @@ import { Command } from 'commander'
 
 import help from './help/index.js'
 
-export default (program: Command): Command => {
-    // attach our custom methods to the program
-    program.configureHelp({
-        formatHelp: (cmd: Command) => help(cmd, program),
+const recursivelyAttach = (cmd: Command) => {
+    // attach our custom methods to the main program
+    cmd.configureHelp({
+        formatHelp: (cmd: Command) => help(cmd, cmd),
     })
+
+    // handle when there are no sub-commands
+    if (cmd.commands.length != 0) {
+        for (const sCmd of cmd.commands) {
+            recursivelyAttach(sCmd)
+        }
+    }
+}
+
+export default (program: Command): Command => {
+    recursivelyAttach(program)
 
     return program
 }
